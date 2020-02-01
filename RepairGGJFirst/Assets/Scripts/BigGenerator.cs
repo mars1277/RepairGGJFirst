f532810b8bc18d7378f3228b26bf1f9c3feea3f1
BarbraTayloer <szabo.barbara.go@gmail.com>
2020-02-01 23:43:07 +0100

    lk5

--- RepairGGJFirst/Assets/Scripts/BigGenerator.cs
+++ RepairGGJFirst/Assets/Scripts/BigGenerator.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BigGenerator : MonoBehaviour
+public class BigGenerator : InteractableObject
 {
+    public Transform holder;
+    public static BigGenerator Instance;
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
