f2d99d0b4cb704f320ef53018b285a4adb9b8195
BarbraTayloer <szabo.barbara.go@gmail.com>
2020-02-01 19:48:21 +0100

    spawny

---
+++ RepairGGJFirst/Assets/Scripts/Barrel_spawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel_spawn : MonoBehaviour
+
+{
+
+    public Transform[] barrelSpawnPoints = new Transform[10];
+    public GameObject[] items = new GameObject[1];
+
+    void Start()
+    {
+        SpawnBarrel();
+    }
+
+    public Transform GetBarrelSpawnPoint()
+    {
+        int index = Random.Range(0, barrelSpawnPoints.Length);
+        return barrelSpawnPoints[index];
+    }
+
+    //selects object to spawn
+    public GameObject GetBarrel()
+    {
+        int index = Random.Range(0, items.Length);
+        return items[index];
+    }
+
+    // spawns the random object on the random point
+    public GameObject SpawnBarrel()
+    {
+        Transform spawnPoint = GetBarrelSpawnPoint();
+        //selects the object
+        GameObject barrel = GetBarrel();
+        //creates the object selected on the point selected
+        GameObject c = Instantiate(barrel, spawnPoint.position, spawnPoint.rotation) as GameObject;
+        //spawns the object
+        return c;
+    }
+}
