04588a59987cd1851f9debd1f635d57e77f310f5
BarbraTayloer <szabo.barbara.go@gmail.com>
2020-02-01 21:25:01 +0100

    rgggg

--- RepairGGJFirst/Assets/Scripts/Barrel_spawn.cs
+++ RepairGGJFirst/Assets/Scripts/Barrel_spawn.cs
@@ -9,9 +9,25 @@ public class Barrel_spawn : MonoBehaviour
     public Transform[] barrelSpawnPoints = new Transform[10];
     public GameObject[] items = new GameObject[1];
 
+    public float OilLevel = 1000;
+    public float ConsumeTime = 1.0f;
+
     void Start()
     {
         SpawnBarrel();
+        StartCoroutine(OilConsumer());
+    }
+
+    public IEnumerator OilConsumer()
+    {
+        while (OilLevel > 0)
+        {
+            Debug.Log("Before Waiting 2 seconds");
+            yield return new WaitForSeconds(ConsumeTime);
+            OilLevel = OilLevel - 1;
+            Debug.Log("After Waiting 2 Seconds");
+        }
+        
     }
 
     public Transform GetBarrelSpawnPoint()
@@ -31,11 +47,8 @@ public class Barrel_spawn : MonoBehaviour
     public GameObject SpawnBarrel()
     {
         Transform spawnPoint = GetBarrelSpawnPoint();
-        //selects the object
         GameObject barrel = GetBarrel();
-        //creates the object selected on the point selected
         GameObject c = Instantiate(barrel, spawnPoint.position, spawnPoint.rotation) as GameObject;
-        //spawns the object
         return c;
     }
 }
