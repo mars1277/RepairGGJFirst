82202f6bb4b8880ec6bfcee3e2b2fb6d2cbac3e6
BarbraTayloer <szabo.barbara.go@gmail.com>
2020-02-01 22:37:28 +0100

    z

---
+++ RepairGGJFirst/Assets/Scripts/OilBarrel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilBarrel : MonoBehaviour
+{
+
+    public float OilLevel = 10;
+    public bool Inserted = false;
+
+    void Update()
+    {
+
+        if (OilLevel > 0)
+        {
+            OilLevel = OilLevel - 1 * Time.deltaTime;
+        }
+
+        if(OilLevel <= 0)
+        {
+        Destroy(gameObject);
+            Debug.Log("destroy");
+        }
+    }
+}
