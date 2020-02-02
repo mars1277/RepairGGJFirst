a3ae520aa2bb33076a16eb0dba2fd2b3491f252a
BarbraTayloer <szabo.barbara.go@gmail.com>
2020-02-02 16:45:57 +0100

    jokvagyunk2

---
+++ RepairGGJFirst/Assets/CanvasManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+
+    public static CanvasManager Instance;
+    public GameObject barrelFeedback;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ShowBarrelFeedback()
+    {
+        StartCoroutine(DelayIE());
+    }
+
+    IEnumerator DelayIE()
+    {
+        barrelFeedback.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        barrelFeedback.SetActive(false);
+
+    }
+}
--- RepairGGJFirst/Assets/Scripts/OilBarrel.cs
+++ RepairGGJFirst/Assets/Scripts/OilBarrel.cs
@@ -12,13 +12,10 @@ public class OilBarrel : InteractableObject
     public bool Inserted = false;
     public bool OnHold = false;
 
-    public GameObject Fuelized;
-    
-
 
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -28,13 +25,13 @@ public class OilBarrel : InteractableObject
             return;
         }
 
-            if (OilLevel > 0)
+        if (OilLevel > 0)
         {
             OilLevel = OilLevel - 1 * Time.deltaTime;
-            Debug.Log("decrease level");
+
         }
 
-        if(OilLevel <= 0)
+        if (OilLevel <= 0)
         {
             Debug.Log("destroy");
             Destroy(gameObject);
@@ -53,8 +50,11 @@ public class OilBarrel : InteractableObject
                 if (!BigGenerator.Instance.CurrentBarrel && Vector3.Distance(PlayerMovement.Instance.transform.position, BigGenerator.Instance.transform.position) < PlayerMovement.Instance.maximumInteractionDistance)
                 {
                     transform.SetParent(BigGenerator.Instance.transform);
-                    Inserted = true; 
-                   // this.transform.position = new Vector3(-5.58f, 0.15f, 0.35f);
+                    Inserted = true;
+                    CanvasManager.Instance.ShowBarrelFeedback();
+                    Debug.Log("setactive false text");
+
+                    // this.transform.position = new Vector3(-5.58f, 0.15f, 0.35f);
                     this.transform.position = new Vector3(-5.58f + 0.189f, 0.15f, 0.35f);
                     BigGenerator.Instance.CurrentBarrel = this;
                     Barrel_spawn.Instance.SpawnBarrel();
